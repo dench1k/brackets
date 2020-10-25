1fd2af029e03d13204270e1a0238107f0c33cbed
Den Moskvitin <den-lb@ukr.net>
2020-10-25 16:27:42 +0200

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let quotes = [];
+    for (let q = 0; q < bracketsConfig.length; q++) {
+        quotes.push(bracketsConfig[q].join(""));
+    }
+    for (let i = str.length; i >= 0; i--) {
+        for (let j = 0; j < quotes.length; j++) {
+            if (str.includes(quotes[j])) {
+                str = str.replace(quotes[j], "");
+            }
+        }
+    }
+    return str === "";
+};
